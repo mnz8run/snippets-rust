7f75193cff477f5d7af46255819c34543db9db59
mnz8 <mnz8@foxmail.com>
2024-06-18 23:28:59 +0800

    feat: lifetime

--- packages/raw_lifetime/src/main.rs
+++ packages/raw_lifetime/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // let array1 = vec![1, 2, 3];
+    let array1 = vec![];
+    let array2 = vec![4, 5, 6];
+
+    let result = first_of_two(&array1, &array2);
+    println!("The first element is: {}", result);
+}
+
+fn first_of_two<'a, T>(slice1: &'a [T], slice2: &'a [T]) -> &'a T {
+    if !slice1.is_empty() {
+        &slice1[0]
+    } else if !slice2.is_empty() {
+        &slice2[0]
+    } else {
+        panic!("Both slices are empty!")
+    }
 }

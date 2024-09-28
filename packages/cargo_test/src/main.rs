7e92047fd68bb0e13bf5a95beee59d6069a5a7ec
mnz8 <mnz8@foxmail.com>
2024-09-29 00:25:10 +0800

    update: raw test

--- packages/cargo_test/src/main.rs
+++ packages/cargo_test/src/main.rs
@@ -4,15 +4,23 @@ fn main() {
 
 /**
  * 一个单元测试
- * #[cfg(test)] 注解告诉 Rust 只在执行 cargo test 时才编译和运行测试代码，而在运行 cargo build 时不这么做。
- * #[test] 注解告诉 Rust 这是一个测试函数。
+ * `#[cfg(test)]` 注解告诉 Rust 只在执行 cargo test 时才编译和运行测试代码，而在运行 cargo build 时不这么做。
+ * `#[test]` 注解告诉 Rust 这是一个测试函数。
  */
 #[cfg(test)]
 mod unit_tests {
+    // 引入父模块中所有项
     use super::*;
 
     #[test]
     fn unit_run_main() {
         main();
     }
+}
+
+// 不在 mod 中
+#[test]
+fn test_fn() {
+    // 测试代码
+    println!("不在 mod 中");
 }
